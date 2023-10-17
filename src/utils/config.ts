bcb6df8b2d5d96c9f819d5ce7f9eaeb9acef1b51
Mikhail <me@reze.moe>
2023-10-18 02:19:34 +0900

    Update default config once more to be more specific in it's example for database entry, even though it doesn't really need to, because this should not be edited by a user.

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -41,10 +41,10 @@ const defaultConfig = {
     },
     "database": [
         {
-            "channel_id": "000000000",
-            "ping_message": "HELLO @everyone MANGA $1",
-            "manga_id": "000000000",
-            "last_chapter": "000000000",
+            "channel_id": "0000000000000000000000",
+            "ping_message": "HELLO @everyone MANGA",
+            "manga_id": "801513ba-a712-498c-8f57-cae55b38cc92",
+            "last_chapter": 374,
             "ignore": true
         }
     ]
