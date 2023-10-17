35da32427445e8b21795c7306061b3bada290a6a
Mikhail <me@reze.moe>
2023-10-18 02:33:32 +0900

    Separate contents for better styling of the files and easier navigation

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,7 @@
 import { PresetOutput } from '@utils/output';
 import YAML from 'yaml';
 import fs from 'fs';
+import { config as defaultConfig } from '@utils/defaults';
 
 export default class Config
 {
@@ -44,21 +45,3 @@ export default class Config
     }
 }
 
-const defaultConfig = {
-    "mangadex": {
-        "apiToken": ""
-    },
-    "discord": {
-        "token": "",
-        "owner_id": "000000000"
-    },
-    "database": [
-        {
-            "channel_id": "0000000000000000000000",
-            "ping_message": "HELLO @everyone MANGA",
-            "manga_id": "801513ba-a712-498c-8f57-cae55b38cc92",
-            "last_chapter": 374,
-            "ignore": true
-        }
-    ]
-}
---
+++ src/utils/defaults.ts
@@ -0,0 +1,18 @@
+export const config = {
+    "mangadex": {
+        "apiToken": ""
+    },
+    "discord": {
+        "token": "",
+        "owner_id": "000000000"
+    },
+    "database": [
+        {
+            "channel_id": "0000000000000000000000",
+            "ping_message": "HELLO @everyone MANGA",
+            "manga_id": "801513ba-a712-498c-8f57-cae55b38cc92",
+            "last_chapter": 374,
+            "ignore": true
+        }
+    ]
+}
