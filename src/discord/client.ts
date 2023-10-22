a4580e0988671032617112642c9aa6ae62227a32
Mikhail <me@reze.moe>
2023-10-23 08:27:32 +0900

    Add basic discord client class

---
+++ src/discord/client.ts
@@ -0,0 +1,16 @@
+import { Client, Events } from 'discord.js';
+import { discordClientConfig } from '@utils/defaults';
+import { PresetOutput } from '@utils/output';
+
+export default class DiscordClient
+{
+    public constructor(private readonly token: string,
+        private readonly owner_id: string,
+        private readonly client: Client = new Client(discordClientConfig),
+        private readonly Output = new PresetOutput("dsc"))
+    {
+        client.once(Events.ClientReady, c => {
+            Output.Log("Discord client ready.");
+        });
+    }
+}
