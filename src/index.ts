2d69c8714550f06a77dd28e54d0cdda0a686f21e
Mikhail <me@reze.moe>
2023-10-23 08:45:12 +0900

    Replace the naming in the index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import Output from "@utils/output";
 import Config from "@utils/config";
 import MangadexApi from "@mangadex/api";
-import DiscordClient from "@discord/client";
+import DiscordRunner from "@discord/runner";
 
 Output.Log("Starting up...");
 
 const config: Config = new Config();
 const mangadexapi: MangadexApi = new MangadexApi();
-const discord: DiscordClient = new DiscordClient(config.discord.token, config.discord.owner_id);
+const discord: DiscordRunner = new DiscordRunner(config.discord.token, config.discord.owner_id);
 
 (async ()=>
 {
