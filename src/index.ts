6fea678671c717d012e4814973002b5a49125c06
Mikhail <me@reze.moe>
2023-10-23 08:36:00 +0900

    Add discord client to the index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import Output from "@utils/output";
 import Config from "@utils/config";
 import MangadexApi from "@mangadex/api";
+import DiscordClient from "@discord/client";
 
 Output.Log("Starting up...");
 
 const config: Config = new Config();
 const mangadexapi: MangadexApi = new MangadexApi();
+const discord: DiscordClient = new DiscordClient(config.discord.token, config.discord.owner_id);
 
 (async ()=>
 {
